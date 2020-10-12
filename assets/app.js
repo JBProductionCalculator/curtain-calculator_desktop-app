4471e0f0a5f5d0daa9b1c1090d4be4b9c19c0aa5
remakslim <artemystarkovsky@gmail.com>
2020-10-12 20:52:59 +0300

    Версия в которой в таблицу и select-ы данные подтягиваются из БД ,так же при выборе товара и типа товара его стоимость достается из БД и при нажатии на кнопку ПОСЧИТАТЬ стоимость товара за умножается на площадь и получается цена. Тоесть все уже работает , осталось только добавить все товары в БД, и сде, вывести все таблицы в отдельные окна и сделать дизайн. Позже сделать админку для веб версии. Но нужно еще сделать канкулятор , возможно его придется полностью переделывать

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,9 @@
-        let width, height, area, productSelect
+        let width, height, area, productSelect, productPrice
         width = document.querySelector('.priceСalculation input#width')
         height = document.querySelector('.priceСalculation input#height')
         area = document.querySelector('.priceСalculation input#area')
         productSelect = document.querySelector('.productSelect')
+
        
 
 
@@ -18,10 +19,13 @@
             //ФУНКЦИЯ ВЫЧИСЛЕНИЯ ЦЕНЫ
             function priceСalculation(){
 
-
                 document.querySelector('.priceСalculation input#count').addEventListener('click', function (){
                     let productArea = areaCalculation()
-                    console.log(productArea);
+                    let price = productArea * productPrice
+                    document.querySelector('#price').innerHTML = price
+
+                    // console.log(productArea)
+                    // console.log(productPrice)
 
                 })
 
@@ -36,6 +40,8 @@
                     return y
                 }
 
+              
+
             }
             priceСalculation()
 
@@ -43,67 +49,150 @@
 
         // -------------------------------------------------------------------------
         // -------------------------------------------------------------------------
-         // вывод информации в Select с названиями товаров и типами
-            selections(horizontalBlinds)
-
-            function selections(data){
-                let allProducts = data
-
-                createProductSelection(allProducts)
-                function createProductSelection(data){
-
-                    let selectProduct = createSelect(data, 'productName', 'product.name', false)
-                    selectProduct.addEventListener('change', function(){
-                        console.log(this.value)
-                    })
-
-                    // здесь я создаю select Для типов товаров и данные которые я передаю в масиве храняться тольк в первом элементе обьекта
-                    // любой таблицы которую я бы не передава. это все делаетсься для того чтобы после можно было легко создать админку
-                    let selectType = createSelect(data, 'productType', '', ['data[0].openType.copact[2]', 
-                                                                            'data[0].openType.mini[2]', 
-                                                                            'data[0].openType.standard[2]', 
-                                                                            'data[0].openType.maximus[2]', 
-                                                                            'data[0].closedType.uniFlat[2]',
-                                                                            'data[0].closedType.uniFullTurn[2]' ] )                  
-                }
+            // создание тегов select с option и вывод в них информации из базы данных 
+            // а также функция возвращает данные о ценнах из БД которые будут учавствовать в подсчете стоимости
+            let allProducts = horizontalBlinds
+            oneSelect(allProducts, 'productName', 'product.name', false)
+            oneSelect(allProducts, 'productType', false, ['data[0].openType.copact[2]', 
+                                                          'data[0].openType.mini[2]', 
+                                                          'data[0].openType.standard[2]', 
+                                                          'data[0].openType.maximus[2]', 
+                                                          'data[0].closedType.uniFlat[2]',
+                                                          'data[0].closedType.uniFullTurn[2]', ] )
 
 
-                //функция которая создает теги select добавляет в них деги option и все это добаляет в нужное местро в разметке
-                // главное что нужно помнить так это что в функцию createOption() попадают уже готовые данные а не команды
-                // функция eval() текст в команды и эти команды сразу же исполняются и отдают данные. в этом легко запутаться
-
-                function createSelect(data, nameID, commandOne, commandsTwo ){
-                    let select = document.createElement('select')
-                    select.id = nameID
-
-                    if (commandsTwo) {
-                        commandsTwo.forEach(function(command){
-                            console.log(eval(command))
-                            let option = createOption(eval(command))
-                            select.append(option)
-                            productSelect.append(select)
-
-                        })
-                        
-                    }else{
-                        data.forEach(function(product){
-                            let option = createOption(eval(commandOne))
-                            select.append(option)
-                            productSelect.append(select) 
-                        })
-                    }                  
-
-                    return select
-                }
+                
 
 
-                function createOption(name){
-                    let op = document.createElement('option')
-                    op.innerHTML = name
-                    return op
-                }
-            
+
+
+
+            function oneSelect(data, id, oneCommand, commandObject){
+                //созданный выпадающий список
+                let newSelect = createSelect(data, id, oneCommand, commandObject)
+
+
+                //нахождение цены при выборе товара и его типа
+                newSelect.addEventListener('change', function(element){
+                    priceSearch(element,this.value  ,data)
+                })
             }
+
+
+            // фальшивый клик по элементу option для первого запуска всех элементо
+            function fakeClick(data){
+                let fakeSelect = document.querySelector('#productName ')
+                fakeSelect.addEventListener('change', function(element){
+                    priceSearch(element,this.value  ,data)
+                })
+
+                let event = new Event('change')                
+                fakeSelect.dispatchEvent(event)
+            }
+            
+            
+
+
+            //функция которая выдает число(цену) при выборе одного из товаров и одной из категорий товаро
+            // параметры функции: 1. весь тег select, 2. текст который в нажатом теге option, 3. весь масив информации
+            //последний параметр передается чтобы сделать так чтобы функция сработала первый раз без события onchange
+            let oneGoods  
+            function priceSearch(select,nameOption,data) {
+
+                if (select.target.id === 'productName') {
+                    data.forEach(function(product){
+                        if (nameOption === product.name) {
+                            // console.log(product)
+                            oneGoods = product
+                            console.log(oneGoods)
+                        }                        
+                    })      
+                }
+
+                if(select.target.id === 'productType'){
+                    // console.log(oneGoods.openType.copact[2])
+                    console.log(oneGoods)
+                    console.log(nameOption)
+                    // console.log(oneGoods.closedType.uniFlat[2])
+
+                   
+                    if(nameOption == oneGoods.openType.copact[2]){
+                        console.log(oneGoods.openType.copact[0])
+                         productPrice = oneGoods.openType.copact[0]
+                    }
+
+                    if(nameOption == oneGoods.openType.mini[2]){
+                        console.log(oneGoods.openType.mini[0])
+                         productPrice = oneGoods.openType.mini[0]
+                    } 
+
+                    if(nameOption == oneGoods.openType.standard[2]){
+                        console.log(oneGoods.openType.standard[0])
+                         productPrice = oneGoods.openType.standard[0]
+                    }
+
+                    if(nameOption == oneGoods.openType.maximus[2]){
+                        console.log(oneGoods.openType.maximus[0])
+                         productPrice = oneGoods.openType.maximus[0]
+                    }
+
+                    if(nameOption == oneGoods.closedType.uniFlat[2]){
+                        console.log(oneGoods.closedType.uniFlat[0])
+                         productPrice = oneGoods.closedType.uniFlat[0]
+                    }
+
+                    if(nameOption == oneGoods.closedType.uniFullTurn[2]){
+                        console.log(oneGoods.closedType.uniFullTurn[0])
+                         productPrice = oneGoods.closedType.uniFullTurn[0]
+                    }
+                    
+                    // return productPrice
+
+
+                }      
+            }
+
+            fakeClick(allProducts)
+                
+            
+               
+            
+               
+            //функция которая создает теги select добавляет в них деги option и все это добаляет в нужное местро в разметке
+            // главное что нужно помнить так это что в функцию createOption() попадают уже готовые данные а не команды
+            // функция eval() текст в команды и эти команды сразу же исполняются и отдают данные. в этом легко запутаться
+            function createSelect(data, nameID, oneCommand, commandObject ){
+                let select = document.createElement('select')
+                select.id = nameID
+
+                if (commandObject) {
+                    commandObject.forEach(function(command){
+                        // console.log(eval(command))
+                        let option = createOption(eval(command))
+                        select.append(option)
+                        productSelect.append(select)
+
+                    })
+                    
+                }else{
+                    data.forEach(function(product){
+                        let option = createOption(eval(oneCommand))
+                        select.append(option)
+                        productSelect.append(select) 
+                    })
+                }                  
+
+                return select
+            }
+
+
+            function createOption(name){
+                let op = document.createElement('option')
+                op.innerHTML = name
+                return op
+            }
+            
+        
 
 
               
--- assets/database.js
+++ assets/database.js
@@ -2,29 +2,29 @@ let horizontalBlinds = [
 
   
    {
-    "name": "Марсесль <span class='red'> АКЦИЯ!!!</span>",
+    "name": "Марсесль АКЦИЯ!!!",
     "openType" : {
-      "copact" :[ 8.3  ,'+1', 'Compact и  фалш шторы'],
+      "copact" :[ 8.3  ,'+1', 'Compact и фалш шторы'],
       "mini" :[ 9.3 ,'+1' , 'MINI'],
       "standard" :[ 11.0 ,'+1' , 'STANDART'],
       "maximus" : [17.0,  '' , 'MAXIMUS'] 
     },
     "closedType" : {
-       "uniFlat" : [17.0 , '+2' , 'UNI  Плоскю Направл'] ,
-       "uniFullTurn" : [23.0 , '+3' , 'UNI  П-обрю Направл.'],
+       "uniFlat" : [17.0 , '+2' , 'UNI Плоскю Направл'] ,
+       "uniFullTurn" : [23.0 , '+3' , 'UNI П-обрю Направл.'],
     }
   },
   {
     "name": "LEN",
     "openType" : {
-      "copact" : [9.3 , '+1' ],
-      "mini" : [10.3 , '+1' ],
-      "standard" :[ 12.0 , '+1' ],
-      "maximus" : 19.0,
+      "copact" : [9.3 , '+1', 'Compact и фалш шторы' ],
+      "mini" : [10.3 , '+1', 'MINI'],
+      "standard" :[ 12.0 , '+1' , 'STANDART' ],
+      "maximus" : [19.0, ' ', 'MAXIMUS'],
     },
     "closedType" : {
-       "uniFlat" :[ 19.0 , '+2' ],
-       "uniFullTurn" : [25.0 , '+3'   ],
+       "uniFlat" :[ 19.0 , '+2', 'UNI Плоскю Направл' ],
+       "uniFullTurn" : [25.0 , '+3' , 'UNI П-обрю Направл.'],
     }
   },
 
